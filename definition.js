ed727dff16f5670a75d3816f9c1e1d1cca837f7e
KIEN20030806 <kien08062003@gmail.com>
2024-11-07 22:01:13 +0700

    o

--- definition.js
+++ definition.js
@@ -128,7 +128,7 @@ Blockly.Python['change_relay_address'] = function (block) {
   code += `  relay_${old_address} = RelayController(${old_address})\n`;
   code += `relay_${old_address}.change_relay_address(${new_address})\n`;
   code += `relay_${new_address} = RelayController(${new_address})\n`;
-  code += `time.sleep_ms(10)\n`;
+  code += `time.sleep_ms(100)\n`;
   return code;
 };
 
